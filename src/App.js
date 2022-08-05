1e8809354b515c498b4eaeda15cdb56bea16c43c
james etchberger <etchbergerjames@gmail.com>
2022-08-05 18:04:38 -0400

    made a counter with a pig as the backgeround and messed around with the css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const adding = () => setCount(count + 1)
+  const addingTen = () => setCount(count + 10)
+  // const fakesubtracting = () => setCount(count > 0? count++: 0)
+  const subtracting = () => setCount(count - 1)
+  const subtractingTen = () => setCount(count - 10)
+  const reset = () => setCount(0)
+  const Checkcount = () => {
+    if (count>0){
+      return subtracting()
+    } else {
+      return reset()
+    }
+  }
+  const Checkcount10 = () => {
+    if (count>10){
+      return subtractingTen()
+    } else {
+      return reset()
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <menubar>
+      
+    </menubar>
+
+
+
+    <body>
+    <header className='Count'>
+    <h1>Count: {count}</h1>
+    </header>
+    
+    <main className='Main'>
+    <button onClick={Checkcount10}>Click -10</button>
+    <button onClick={Checkcount}>Click Down</button>
+    <button onClick={reset}> Click Reset</button>
+    <button onClick={adding}>Click Up</button>
+    <button onClick={addingTen}>Click +10</button>
+    </main>
+    </body>
+    </>
   );
 }
 
